98e584891cbb41b80908876c9dcb5175a7ced5bc
kimtaehyung <xoguddkenl@gmail.com>
2024-11-01 14:39:50 +0900

    - 모달 오버레이 추가 - 북마크 페이지 반응형 적용

--- src/app/(thgallery)/_components/_main/Modal.tsx
+++ src/app/(thgallery)/_components/_main/Modal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../../../_styles/modal.module.scss";
 import { Photo } from "../../_model/photos";
 import Image from "next/image";
@@ -19,52 +19,77 @@ const Modal = ({
 }) => {
   const { heart, toggle } = useHeartToggle(image, updateStorageData);
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
   return (
-    <div className={styles.modal_wrap}>
-      <div className={styles.image_area}>
-        <Image
-          className={styles.image}
-          src={image.urls.regular}
-          alt={image.alt_description}
-          fill
-          sizes="(min-width: 60em) 24vw,
-          (min-width: 28em) 45vw,
-          100vw"
-          priority={true}
-        />
-      </div>
-      <div className={styles.description_area}>
-        {image.alt_description ? <p>제목 : {image.alt_description}</p> : ""}
-        {image.description ? <p>설명 : {image.description}</p> : ""}
-
-        <p>작가 : {image.user.first_name}</p>
-        {image.user.instagram_username ? (
-          <p>
-            instagram :
-            <Link
-              href={`https://www.instagram.com/${image.user.instagram_username}/`}
-            >
-              {image.user.instagram_username}
-            </Link>
-          </p>
-        ) : (
-          ""
-        )}
-        <p>likes : {image.likes}</p>
-      </div>
-
-      <div className={styles.btn_area}>
-        <button className={styles.like_btn} onClick={toggle}>
-          <FontAwesomeIcon
-            icon={faHeart}
-            id={heart ? styles.like : styles.unlike}
+    <>
+      <div className={styles.modal_overay} onClick={onClose}></div>
+      <div className={styles.modal_wrap}>
+        <div className={styles.image_area}>
+          <Image
+            className={styles.image}
+            src={image.urls.regular}
+            alt={image.alt_description}
+            fill
+            sizes="(max-width: 768px) 100vw,
+          (max-width: 1280px) 80vw,
+          50vw"
+            priority={true}
           />
-        </button>
-        <button onClick={onClose} className={styles.close_btn}>
-          <FontAwesomeIcon icon={faX} className={styles.close_icon} />
-        </button>
+        </div>
+        <div className={styles.description_area}>
+          <ul>
+            {image.alt_description && (
+              <li>
+                <span>제목:</span> {image.alt_description}
+              </li>
+            )}
+            {image.description && (
+              <li>
+                <span>설명:</span> {image.description}
+              </li>
+            )}
+            {image.user.first_name && (
+              <li>
+                <span>작가:</span> image.user.first_name
+              </li>
+            )}
+            {image.user.instagram_username && (
+              <li>
+                <span>인스타그램:</span>{" "}
+                <Link
+                  href={`https://www.instagram.com/${image.user.instagram_username}/`}
+                >
+                  {image.user.instagram_username}
+                </Link>
+              </li>
+            )}
+            {image.likes && (
+              <li>
+                <span>좋아요:</span> {image.likes}
+              </li>
+            )}
+          </ul>
+        </div>
+
+        <div className={styles.btn_area}>
+          <button className={styles.like_btn} onClick={toggle}>
+            <FontAwesomeIcon
+              icon={faHeart}
+              id={heart ? styles.like : styles.unlike}
+            />
+          </button>
+          <button onClick={onClose} className={styles.close_btn}>
+            <FontAwesomeIcon icon={faX} className={styles.close_icon} />
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
